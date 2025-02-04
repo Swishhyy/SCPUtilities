3f24203f2dd38b48edbab51b20b8e807cbe5e96a
Swishhyy <72903966+Swishhyy@users.noreply.github.com>
2025-02-04 11:03:17 -0600

    Update SCP079Extensions.cs

--- SCPUtils/Extensions/SCP079Extensions.cs
+++ SCPUtils/Extensions/SCP079Extensions.cs
@@ -1,4 +1,5 @@
-﻿using Exiled.API.Features;
+﻿using System.Reflection;
+using Exiled.API.Features;
 using Exiled.API.Features.Roles;
 
 namespace SCPUtils.Extensions
@@ -6,12 +7,25 @@ namespace SCPUtils.Extensions
     public static class Scp079Extensions
     {
         /// <summary>
-        /// Dummy implementation to simulate setting the AP charge rate for SCP-079.
-        /// Replace with actual logic if available.
+        /// Sets the AP charge rate for SCP‑079 by updating its private "apChargeRate" field.
         /// </summary>
+        /// <param name="role">The SCP‑079 role instance.</param>
+        /// <param name="chargeRate">The new AP charge rate value.</param>
         public static void SetAPChargeRate(this Scp079Role role, float chargeRate)
         {
-            Log.Info($"[SCPUtils] (Dummy) Setting SCP-079's AP charge rate to {chargeRate}");
+            // Attempt to retrieve the private field "apChargeRate" using reflection.
+            FieldInfo? field = role.GetType().GetField("apChargeRate", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (field != null)
+            {
+                // If found, set its value to the provided chargeRate.
+                field.SetValue(role, chargeRate);
+                Log.Info($"[SCPUtils] Set SCP‑079's AP charge rate to {chargeRate}");
+            }
+            else
+            {
+                // Log a warning if the field cannot be found.
+                Log.Warn("[SCPUtils] Could not set SCP‑079's AP charge rate; field 'apChargeRate' not found.");
+            }
         }
     }
 }
