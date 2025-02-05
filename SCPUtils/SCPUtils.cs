592223d52a3d7154a3f3458c67b88f08c3ddf3b3
Swishhyy <dylan.minatrea@gmail.com>
2025-02-04 23:23:54 -0600

    Fixed config generation issues

--- SCPUtils/SCPUtils.cs
+++ SCPUtils/SCPUtils.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Exiled.API.Features;
 using Exiled.Events.EventArgs.Player;
 using SCPUtils.Editors;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 using Player = Exiled.Events.Handlers.Player;
 
 namespace SCPUtils
@@ -14,13 +17,19 @@ namespace SCPUtils
         public override Version Version => new Version(1, 0, 0);
 
         private readonly List<IScpEditor> scpEditors = new List<IScpEditor>();
+        private const string ConfigFilePath = "config.yml"; // Default Exiled config file
 
         public override void OnEnabled()
         {
+            LoadOrCreateConfig();
             InitializeEditors();
             Player.Verified += OnPlayerVerified;
 
-            Log.Info($"{Name} v{Version} by {Author} has been enabled!");
+            if (Config.Debug)
+                Log.Info($"{Name} v{Version} by {Author} has been enabled with debug mode!");
+            else
+                Log.Info($"{Name} v{Version} by {Author} has been enabled!");
+
             base.OnEnabled();
         }
 
@@ -52,6 +61,77 @@ namespace SCPUtils
             {
                 editor.TryEdit(ev, Config);
             }
+        }
+
+        private void LoadOrCreateConfig()
+        {
+            if (!File.Exists(ConfigFilePath))
+            {
+                Log.Warn("[SCPUtils] Config file not found. Creating a new one...");
+                SaveConfig(Config); // Save the default config
+            }
+            else
+            {
+                try
+                {
+                    var deserializer = new DeserializerBuilder()
+                        .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                        .Build();
+
+                    var yamlContent = File.ReadAllText(ConfigFilePath);
+                    var fullConfig = deserializer.Deserialize<Dictionary<string, Config>>(yamlContent);
+
+                    if (fullConfig != null && fullConfig.ContainsKey("SCPUtils"))
+                    {
+                        var loadedConfig = fullConfig["SCPUtils"];
+
+                        // Update properties of the existing Config object
+                        UpdateConfig(loadedConfig);
+
+                        Log.Info("[SCPUtils] Configuration loaded successfully.");
+                    }
+                    else
+                    {
+                        Log.Warn("[SCPUtils] 'SCPUtils' section missing. Adding defaults...");
+                        SaveConfig(Config); // Save current default config
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Error($"[SCPUtils] Error reading config file: {ex.Message}");
+                    SaveConfig(Config); // Backup strategy in case of errors
+                }
+            }
+        }
+
+        private void UpdateConfig(Config loadedConfig)
+        {
+            // Use reflection to copy all properties from the loaded config
+            foreach (var property in typeof(Config).GetProperties())
+            {
+                if (property.CanWrite)
+                {
+                    var value = property.GetValue(loadedConfig);
+                    property.SetValue(Config, value);
+                }
+            }
+        }
+
+        private void SaveConfig(Config config)
+        {
+            var serializer = new SerializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+
+            var fullConfig = new Dictionary<string, Config>
+            {
+                { "SCPUtils", config }
+            };
+
+            var yamlContent = serializer.Serialize(fullConfig);
+            File.WriteAllText(ConfigFilePath, yamlContent);
+
+            Log.Info("[SCPUtils] Default configuration saved successfully.");
         }
     }
 }
--- SCPUtils/obj/x64/Debug/net8.0/SCPUtils.AssemblyInfo.cs
+++ SCPUtils/obj/x64/Debug/net8.0/SCPUtils.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SCPUtils")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22104b4f997fccf00b2e0b1b1a6067a44995a579")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a32c8f197cb010981237a2920ca4cd9ee7a680b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SCPUtils")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SCPUtils")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
