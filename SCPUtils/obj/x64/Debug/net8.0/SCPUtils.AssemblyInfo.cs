1a32c8f197cb010981237a2920ca4cd9ee7a680b
Swishhyy <dylan.minatrea@gmail.com>
2025-02-04 23:14:17 -0600

    Fixed .sln AGAIN....

--- SCPUtils/obj/x64/Debug/net8.0/SCPUtils.AssemblyInfo.cs
+++ SCPUtils/obj/x64/Debug/net8.0/SCPUtils.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SCPUtils")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d6f638ecbeae8560591110e2f6f4903dc93e2c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22104b4f997fccf00b2e0b1b1a6067a44995a579")]
 [assembly: System.Reflection.AssemblyProductAttribute("SCPUtils")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SCPUtils")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
